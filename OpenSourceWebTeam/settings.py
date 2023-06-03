ef7d5494b533c775e126462fc8348f9bec2f48f9
강명석 <k4858678@gmail.com>
2023-06-03 20:07:50 +0900

    게임 탭 수정 완료 로그인 기능 테스트 완료

--- OpenSourceWebTeam/settings.py
+++ OpenSourceWebTeam/settings.py
@@ -140,3 +140,7 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#로그인 세팅
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
