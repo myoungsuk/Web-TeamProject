3840b5644c4ea7fd1f9105cdc83e8eacac1a40e1
LikeNoru <stkjas4657@naver.com>
2023-06-12 01:19:56 +0900

    section contents 1

---
+++ introduce_app/static/js/map.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+        let mapContainer = document.getElementById("map");
+        let options = {
+            center: new kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3
+        };
+        let map = new kakao.maps.Map(mapContainer, options);
+
+    }
+);
