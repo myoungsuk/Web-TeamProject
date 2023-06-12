acd18fd1d4682a43b4f8e381205f593b1655f840
LikeNoru <stkjas4657@naver.com>
2023-06-13 05:51:39 +0900

    designed profile-2

--- introduce_app/static/js/scroll.js
+++ introduce_app/static/js/scroll.js
@@ -4,22 +4,20 @@ $(document).ready(function () {
     }, {passive: false});
     let page = 1;
     const lastPage = $('.scroll-section').length;
-    let nextPageHeight = $('.scroll-section')[page].height;
 
-    $("html").animate({scrollTop: 0}, 10);
+    $("html").animate({scrollTop: 0}, 10, 'easeOutBounce');
 
     $(window).on("wheel", function (e) {
         if ($("html").is(":animated")) return;
 
         if (e.originalEvent.deltaY > 0) {
             if (page == lastPage) return;
-
             page++;
         } else if (e.originalEvent.deltaY < 0) {
             if (page == 1) return;
-
             page--;
         }
+
         let posTop = (page - 1) * $().height();
 
         $("html").animate({scrollTop: posTop});
